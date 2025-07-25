165b34d785dc2da9e30ac69a05228ec910b528e2
Sourashis Pal <palsourashis04@gmail.com>
2025-07-25 10:46:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const textBox = document.getElementById("textBox");
+const toFahrenhit = document.getElementById("toFahrenhit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
+function convert(){
+    if(toFahrenhit.checked){
+        temp = Number(textBox.value);
+        temp = (temp * (9/5)) + 32;
+        result.textContent = temp + "°F"
+    }
+    else if(toCelsius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * 5/9;
+        result.textContent = temp + "°C"
+    } 
+    else{
+        result.textContent = `First select an unit`
+    }
+}
